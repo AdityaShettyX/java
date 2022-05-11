fac3e214a207225e875b169145e7e0d01f71385a
AdityaShettyX <105192774+AdityaShettyX@users.noreply.github.com>
2022-05-11 10:28:10 +0530

    Add files via upload

---
+++ Prcatical/AllArithmatic.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class AllArithmatic {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=2;
+		int c=a+b;
+		
+		System.out.println(c);
+		System.out.println(a-b);
+		System.out.println(a/b);
+		System.out.println(a*b);
+		System.out.println(a%b);
+		
+	}
+
+}
