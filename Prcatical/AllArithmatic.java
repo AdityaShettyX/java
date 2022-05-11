a18cb011871146d672ac8295bec6cfb93cf0f7cb
AdityaShettyX <105192774+AdityaShettyX@users.noreply.github.com>
2022-05-11 11:09:41 +0530

    Update AllArithmatic.java

--- Prcatical/AllArithmatic.java
+++ Prcatical/AllArithmatic.java
@@ -7,11 +7,11 @@ public class AllArithmatic {
 		int b=2;
 		int c=a+b;
 		
-		System.out.println(c);
-		System.out.println(a-b);
-		System.out.println(a/b);
-		System.out.println(a*b);
-		System.out.println(a%b);
+		System.out.println("Addition of a+b is " +c);
+		System.out.println("subraction of a and b is " +a-b);
+		System.out.println("division of and b is " +a/b);
+		System.out.println("multiplication of a and b is " +a*b);
+		System.out.println("modulus of a and b is " +a%b);
 		
 	}
 
