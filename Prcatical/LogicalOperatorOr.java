9211f1410f20f5781ab54ac7b7c76e544e21ec99
AdityaShettyX <105192774+AdityaShettyX@users.noreply.github.com>
2022-05-18 12:21:46 +0530

    Add files via upload

---
+++ Prcatical/LogicalOperatorOr.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class LogicalOperatorOr {
+	public static void main(String[]args) {
+		 //intialising variables
+        int a=10, b=20, c=30, d=0;
+
+        //a, b, c displaying
+        System.out.println("Value of a: "+ a);
+        System.out.println("Value of b: "+ b);
+        System.out.println("Value of c: "+ c);
+
+        //using logical AND to Verify Two constraints
+        if((a<b) || (b==c)) {
+
+            d=a+b+c;
+            System.out.println("Sum is: " + d);
+        }
+ 
+        else 
+            System.out.println("False Condition");
+	}
+
+}
