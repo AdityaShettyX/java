84ca33833ea457dce583acc1405a78c1462b17cc
AdityaShettyX <105192774+AdityaShettyX@users.noreply.github.com>
2022-05-18 12:23:33 +0530

    Add files via upload

---
+++ Prcatical/LogicalOperatorNot.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class LogicalOperatorNot {
+	public static void main(String[]args) {
+		 //intialising variables
+        int a=10, b=20;
+
+        //a, b, c displaying
+        System.out.println("Value of a: "+ a);
+        System.out.println("Value of b: "+ b);
+        
+
+        System.out.println("!(a < b) = " + !(a < b));
+        System.out.println("!(a > b) = " + !(a > b));
+	}
+
+}
